c68b6cc0da8cbb17df19a3d99f42853262d1dedf
SOC <SOC@IED310-PC10>
2023-02-13 17:12:01 +0100

    add

--- Botiga Objectes/Botiga.cs
+++ Botiga Objectes/Botiga.cs
@@ -8,56 +8,35 @@ namespace Botiga_Objectes
 {
     class Botiga
     {
-
-
         public string Nom { get; set; }
-
         public Producte[] ProductesBotiga;
-
         private int Nelements;
-
-
-
         public Botiga(int num)
         {
             ProductesBotiga = new Producte[num];
             Nelements = 0;
-
-
         }
-
         public bool AfegirProducte(Producte Producte)
         {
             bool afegit = false;
             bool trobat = false;
             int posBuscar=0;
-            
-
             while((ProductesBotiga[posBuscar] != null) || (trobat==false)&& (posBuscar<ProductesBotiga.Length))
             {
                 if (ProductesBotiga[posBuscar] == null)
                 {
                     trobat = true;
                 }
-
                 else
                 posBuscar++;
-
-
             }
-
             if (trobat) {
                 ProductesBotiga[posBuscar] = Producte;
                 afegit = true;
                 Nelements++;
             }
-
-          
             return afegit;
-
-
         }
-
         public int BuscarProducte(Producte producte)
         {
             int pos = -1;
@@ -68,22 +47,15 @@ namespace Botiga_Objectes
                 {
                     cont++;
                 }
-
-
                 else if (ProductesBotiga[i].Nom == producte.Nom)
                 {
                     pos = cont;
                     cont++;
                 }
-
                 cont++;
-                
-
             }
           return pos;
-
         }
-
         private int BuscarProducte(string producte)
         {
             int pos = -1;
@@ -94,24 +66,15 @@ namespace Botiga_Objectes
                 {
                     cont++;
                 }
-
-
                 else if (ProductesBotiga[i].Nom == producte)
                 {
                     pos = cont;
                     cont++;
                 }
-
                 cont++;
-
             }
-
-
-
             return pos;
-
         }
-
         public Producte TornarProducte(string nom)
         {
             int cont = 0;
@@ -120,49 +83,32 @@ namespace Botiga_Objectes
             {
                 if (ProductesBotiga[i] == null)
                 {
-
                 }
-
-
                 else if (ProductesBotiga[i].Nom == nom)
                 {
                     pos = cont;
                     cont++;
                 }
-
                 cont++;
-
-
             }
             return ProductesBotiga[pos];
         }
-
         //ModificarProducte(Producte producte, String nou_nom, double nou_preu) : Mètode
         //    públic.Donat un producte hem de buscar-lo a la taula de productes per saber
         //    quina posició de la taula ocupa, per així
         //    després modificar les dades. Retorna un booleà amb true si l’ha modificat i false si ho ha pogut.
-
         public bool ModificarProducte(Producte producte, string nomNou, double preuNou, double ivaNou)
         {
             bool trobat = false;
-
-            
-                
                 producte.Nom = nomNou;
                 producte.Preu_sense_iva = preuNou;
                 producte.Iva = ivaNou;
                 trobat = true;
-            
-
             return trobat;
-
-
         }
-
         //public bool ModificarProducte(string producte, string nomNou, double preuNou)
         //{
         //    bool trobat = false;
-
         //    if (BuscarProducte(producte) != -1)
         //    {
         //        int pos = BuscarProducte(producte);
@@ -170,22 +116,15 @@ namespace Botiga_Objectes
         //        ProductesBotiga[pos].Preu = preuNou;
         //        trobat = true;
         //    }
-
         //    return trobat;
-
-
         //}
-
         //EsborrarProducte(Producte producte) : Mètode públic.Donat un producte hem de buscar-lo a
         //    la taula de productes per saber quina posició de la taula ocupa, 
         //    per així després esborrar-lo.Per esborrar-lo de la taula, 
         //    posarem null en el lloc de la taula que s’hagi trobat.
-
-
         public bool EsborrarProducte(Producte producte)
         {
             bool esborrat = false;
-
             if (BuscarProducte(producte) != -1)
             {
                 int pos = BuscarProducte(producte);
@@ -193,16 +132,11 @@ namespace Botiga_Objectes
                 esborrat = true;
                 Nelements--;
             }
-
-
-
             return esborrat;
         }
-
         //PrestatgeLliure() : Mètode privat.Retorna la 
         //    posició de la taula a on es pot afegir un producte. 
         //    Retorna -1 si la botiga està plena.
-
         public int PrestatgeLLiure()
         {
             int i = 0;
@@ -215,29 +149,18 @@ namespace Botiga_Objectes
                     trobat = true;
                     pos = i;
                 }
-
                 else
                     i++;
-
-
             }
-
             return pos;
-        
-
-
         }
-
         //ToString(): retorna de forma amigable un string amb tots els productes amb 
         //    els seus preus i iva respectius. 
         //    Recorda que no es pot fer servir el Console.Write 
         //    dintre de l’objecte, el Console.Write s’ha de fer servir des del main().
-
         public string BotigaText()
         {
             string botigaText = "";
-
-
             for (int i = 0; i < ProductesBotiga.Length; i++)
             {
                 if (ProductesBotiga[i] != null)
@@ -246,16 +169,8 @@ namespace Botiga_Objectes
                     botigaText += new string(' ', Console.WindowWidth / 3) + $"Preu: {ProductesBotiga[i].Preu_sense_iva}€ \n";
                     botigaText += new string(' ', Console.WindowWidth / 3) + $"IVA: {ProductesBotiga[i].Iva}%\n";
                 }
-
-
             }
-
-
             return botigaText;
-
-
-
-
         }
     }
 }
--- Botiga Objectes/Cistella.cs
+++ Botiga Objectes/Cistella.cs
@@ -10,38 +10,23 @@ namespace Botiga_Objectes
 {
     class Cistella
     {
-
         public string Data { get; set; }
-
         public Producte[] ProductesCistella;
-
         private int NelementsCistella;
-
         public double Moneder;
-
-
         public Cistella( int productes, double diners)
         {
-            
             Moneder = diners;
             ProductesCistella = new Producte[productes];
             NelementsCistella = 0;
-
         }
-
         //       ComprarProducte(Producte producte, int quantitat) : Afegeix un producte
         //       tantes vegades com indiqui quantitat.
-
-
         public bool ComprarProducte(Producte producte, int quantitat)
         {
             // 0 = no hi ha diners, 1=afegit, -1 No afegit per espai.
-            
-            
             bool trobat = false;
             int posBuscar = 0;
-            
-
             if (Moneder - producte.PreuProducte() * quantitat >= 0 && quantitat<ProductesCistella.Length)
             {
                 for (int j = 0; j < quantitat && posBuscar < ProductesCistella.Length; j++)
@@ -55,78 +40,49 @@ namespace Botiga_Objectes
                             p.Nom = producte.Nom;
                             p.Preu_sense_iva = producte.Preu_sense_iva;
                             p.Iva = producte.Iva;
-
                             ProductesCistella[posBuscar] = p;
                             NelementsCistella++;
                             trobat = true;
                             fi = true;
-                            
                         }
                         else
                         {
                             posBuscar++;
                             if (posBuscar >= ProductesCistella.Length)
                             {
-                                
                                 fi = true;
                             }
                         }
                     }
                 }
             }
-            
-
             Moneder = Math.Round((Moneder - producte.PreuProducte() * quantitat), 2);
             return trobat;
-
-
         }
-
         public double cistellaCostTotal()
         {
             double costTotal=0;
-
             for (int i = 0; i < NelementsCistella; i++) {
-
                 costTotal += ProductesCistella[i].Preu_sense_iva + (ProductesCistella[i].Preu_sense_iva * ProductesCistella[i].Iva/100);
-                
-
                     }
-
-
             return costTotal;
         }
-
-        
-
-
         //ToString() : retorna de forma amigable un string amb tots els 
         //    productes.Retorna també el total amb iva inclòs.
         //    Recordar que no es pot fer servir el Console.Write 
         //    dintre de l’objecte, el Console.Write s’ha 
         //        de fer servir des del main().
-
         public string CistellaText()
         {
-
             string cistellaText = "";
-
             for(int i=0; i< NelementsCistella; i++)
             {
-
                 cistellaText += new string(' ', Console.WindowWidth / 3) + $"Nom: <{ProductesCistella[i].Nom}>  \n";
                 cistellaText += new string(' ', Console.WindowWidth / 3) + $"Preu: {ProductesCistella[i].Preu_sense_iva}€ \n";
                 cistellaText += new string(' ', Console.WindowWidth / 3) + $"Iva:  {ProductesCistella[i].Iva}% \n";
-
-
-                
             }
-
             cistellaText += $"Cost Total Iva Inclos: {cistellaCostTotal()}";
-
             return cistellaText;
-
         }
-
     }
 }
